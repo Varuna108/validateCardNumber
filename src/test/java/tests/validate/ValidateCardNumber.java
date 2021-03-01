95169cee4e2d8d42e07b1de5615f654a944908fe
Varuna <varuna108@yandex.ru>
2021-03-01 04:20:02 +0300

    "add bin method"

--- src/test/java/tests/validate/ValidateCardNumber.java
+++ src/test/java/tests/validate/ValidateCardNumber.java
@@ -3,31 +3,44 @@ package tests.validate;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static tests.validate.helpers.HelperMethods.stringToArray;
-import static tests.validate.helpers.HelperMethods.moonAlgorithm;
+import static tests.validate.helpers.HelperMethods.*;
 
 @Tag("test")
 public class ValidateCardNumber {
 
-
     @Test
-    void validateCardBankWithMoon() {
-        String result =
+    void validateCardBankAndBinBank() {
+        String resultResponse =
                 when()
                         .get("https://api.randomdatatools.ru/?count=50&params=bankCard")
                         .then()
                         .statusCode(200).extract().response().asString().replaceAll("[^\\p{N}]", "");
 
-        String[] newStrings = stringToArray(result);
+        String[] newStrings = stringToArray(resultResponse);
         for (int i = 0; i < newStrings.length; i++) {
             try {
                 assertThat(moonAlgorithm(newStrings[i]), containsString("Card Valid"));
                 System.out.println("Valid Card: " + newStrings[i] + ", index in the list: " + i);
             } catch (AssertionError ignored) {
             }
+        }
+
+        String[] validCard = arrayValidCard();
+        try {
+            for (int i = 0; i < validCard.length; i++) {
+                System.out.println("Bin bank: " + validCard[i]);
+                when()
+                        .get("https://lookup.binlist.net/" + validCard[i])
+                        .then()
+                        .log().body()
+                        .statusCode(200).extract().response().asString();
+            }
+        } catch (AssertionError ignored) {
         }
     }
 }
--- src/test/java/tests/validate/helpers/HelperMethods.java
+++ src/test/java/tests/validate/helpers/HelperMethods.java
@@ -1,8 +1,11 @@
 package tests.validate.helpers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class HelperMethods {
+    private static final List<String> listOfValidCard = new ArrayList<String>();
 
     public static String moonAlgorithm(String value) {
             int sum1 = 0;
@@ -23,17 +26,22 @@ public class HelperMethods {
             int sum = sum1 + sum2;
             if (value.length() != 16) sum = 1;
             if (sum % 10 == 0) {
+                listOfValidCard.add(value.substring(0, 8));
                 return ("Card Valid" + value);
             } else {
                 return ("Card not Valid" + value);
             }
     }
 
+
     public static String[] stringToArray(String value) {
         String newStr  = value.replaceAll("(.{16})", "$1|");
         String[] newStrings = newStr.split("\\|");
         System.out.println("List of all cards: " + Arrays.toString(newStrings));
         return newStrings;
-
         }
+
+    public static String[] arrayValidCard() {
+        return listOfValidCard.toArray((new String[listOfValidCard.size()]));
+    }
 }
